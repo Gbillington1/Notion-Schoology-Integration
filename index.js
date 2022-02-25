02753927443e027590aea677a1f2c8155ce56b00
Graham Billington <graahamhbillington@gmail.com>
2022-02-24 21:57:37 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
 require('dotenv').config();
-const { Client } = require('@notionhq/client');
-
-const notion = new Client({
-    auth: process.env.NOTION_TOKEN
-});
+const { v4: uuidv4 } = require('uuid');
+const schoology = require('./schoology.js');
+const notion = require('./notion.js');
+const util = require('./util.js');
 
 (async () => {
-    const databaseID = process.env.NOTION_DATABASE_ID;
+    
+    // get events from schoology (7 day range)
+    const events = await schoology.getUserEvents(process.env.SCHOOLOGY_USER_ID);
+    console.log(events)
 
-    // date formatting
-    const datetime = new Date();
-    const adjustedDatetime = new Date(datetime.getTime() - (datetime.getTimezoneOffset() * 60000));
-    const date = adjustedDatetime.toISOString().split('T')[0];
+
+    // const databaseID = process.env.NOTION_DATABASE_ID;
 
     // create a row in the master database
-    // TODO: break this up into functions
-    const response = await notion.pages.create({
-        parent: { database_id: databaseID },
-        properties: {
-            Name: {
-                title: [
-                    {
-                        text: {
-                            content: "Big important deadline",
-                        },
-                    },
-                ],
-            },
-            Database: {
-                select: {
-                    name: "Deadlines",
-                },
-            },
-            Date: {
-                date: {
-                    start: date,
-                },
-            },
-            Priority: {
-                multi_select: [
-                    {
-                        name: "High",
-                    }
-                ]
-            },
-            Project: {
-                relation: [
-                    {
-                        id: "5648a1520bf949e2a6ace594f134f796",
-                    },
-                ],
-            },
-            Status: {
-                select: {
-                    name: "In Progress",
-                },
-            },
-        },
-    });
-
+    // await notion.createRowInMaster(databaseID, "Big important deadline", "Tasks", util.getISODate(), "High", "5648a1520bf949e2a6ace594f134f796", "In Progress");
 })()
