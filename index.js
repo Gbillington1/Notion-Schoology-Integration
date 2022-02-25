c22e05a63ac356eb4fdccbebc7da17beb0565173
Graham Billington <graahamhbillington@gmail.com>
2022-02-24 22:44:27 -0500

    added loop through all schoology events, filter based on type, create Event() and add to an arr

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const { v4: uuidv4 } = require('uuid');
 const schoology = require('./schoology.js');
 const notion = require('./notion.js');
 const util = require('./util.js');
+const { Event } = require('./Event.js');
 
 (async () => {
-    
-    // get events from schoology (7 day range)
-    const events = await schoology.getUserEvents(process.env.SCHOOLOGY_USER_ID);
-    console.log(events)
 
+    // get events from schoology (7 day range)
+    const sgyEvents = await schoology.getUserEvents(process.env.SCHOOLOGY_USER_ID, "2022-02-7");
+
+    let events = [];
+    sgyEvents.forEach((event) => {
+
+        if (event.type === "assignment") {
+            let notionEvent = new Event(
+                event.id,
+                event.title,
+                "Tasks",
+                event.start.split(" ")[0],
+                "Medium",
+                1234,
+                "To Do"
+            );
+            events.push(notionEvent);
+        }
+
+    })
+
+    console.log(events)
 
     // const databaseID = process.env.NOTION_DATABASE_ID;
 
