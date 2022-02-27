4cb8538fc556e1066cb13f41f95a8626b4de9411
Graham Billington <graahamhbillington@gmail.com>
2022-02-26 23:10:23 -0500

    added functionality to skip events that were already added to the notion db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
-    require('dotenv').config({path: '/home/graham/notion-schoology-integration/.env'});
+    require('dotenv').config({ path: '/home/graham/notion-schoology-integration/.env' });
 } else {
-    require('dotenv').config({path: './.env'});
+    require('dotenv').config({ path: './.env' });
 }
 
 const { v4: uuidv4 } = require('uuid');
@@ -12,40 +12,51 @@ const { Event } = require('./Event.js');
 
 (async () => {
 
-    // get events from schoology (7 day range)
+    // get events from schoology (7 day range default)
     const sgyEvents = await schoology.getUserEvents(process.env.SCHOOLOGY_USER_ID);
+
+    // get events from notion (7 day range default)
+    const notionTasksAndDeadlines = await notion.getTasksAndDeadlines()
 
     // get page objects with the tag "Course"
     const notionProjects = await notion.getCourseProjects();
 
-    sgyEvents.forEach(async (event) => {
+    // filter out events that aren't assignments
+    sgyEvents.filter((event) => {
+        return event.type === "assignment";
+    }).forEach(async (event) => {
 
-        if (event.type === "assignment") {
+        // check if event is already in the notion database
+        const duplicateEntry = notionTasksAndDeadlines.find(task => task.properties.Name.title[0]. plain_text === event.title)
 
-            // get title of course from from /sections/{id}
-            const sgyCourse = await schoology.getCourseSection(event.section_id);
-            const sgyCourseTitle = sgyCourse.course_title.replace(/\s+/g, '-');
-
-            // find page in notion that matches the schoology event course
-            const projectPage = notionProjects.find(project => project.url.includes(sgyCourseTitle));
-
-            // create a new event object
-            let notionEvent = new Event(
-                event.id,
-                event.title,
-                "Tasks",
-                event.start.split(" ")[0],
-                "Medium",
-                projectPage.id, 
-                "To Do"
-            );
-
-            // add event to new row in the master DB
-            await notion.createRowInMaster(notionEvent)
-
-            console.log("Successfully added event to Notion: " + notionEvent.title);
-
+        // skip duplicates 
+        if (duplicateEntry) {
+            console.log(`${event.title} already exists in the Notion database. Skipping creation.`);
+            return
         }
+
+        // get title of course from from /sections/{id}
+        const sgyCourse = await schoology.getCourseSection(event.section_id);
+        const sgyCourseTitle = sgyCourse.course_title.replace(/\s+/g, '-');
+
+        // find page in notion that matches the schoology event course
+        const projectPage = notionProjects.find(project => project.url.includes(sgyCourseTitle));
+
+        // create a new event object
+        let notionEvent = new Event(
+            event.id,
+            event.title,
+            "Tasks",
+            event.start.split(" ")[0],
+            "Medium",
+            projectPage.id,
+            "To Do"
+        );
+
+        // add event to new row in the master DB
+        await notion.createRowInMaster(notionEvent)
+
+        console.log("Successfully added event to Notion: " + notionEvent.title);
 
     })
 
