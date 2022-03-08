213dd577bcfb742ceac224ec34ea333335dd026b
Graham Billington <graahamhbillington@gmail.com>
2022-03-08 10:13:40 -0500

    removed methods that were moved to a class, added JSDOC

--- notion.js
+++ notion.js
@@ -5,69 +5,11 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN
 });
 
-// creates a row in the master database
-async function createRowInMaster(event) {
-    const response = await notion.pages.create({
-        parent: { database_id: process.env.NOTION_MASTER_DATABASE_ID },
-        properties: {
-            Name: {
-                title: [
-                    {
-                        text: {
-                            content: event.title,
-                        },
-                    },
-                ],
-            },
-            Database: {
-                select: {
-                    name: event.database,
-                },
-            },
-            Date: {
-                date: {
-                    start: event.date,
-                },
-            },
-            Priority: {
-                multi_select: [
-                    {
-                        name: event.priority,
-                    }
-                ]
-            },
-            Project: {
-                relation: [
-                    {
-                        id: event.projectPageID,
-                    },
-                ],
-            },
-            Status: {
-                select: {
-                    name: event.status,
-                },
-            },
-        },
-    });
-}
-
-// find section ID's for every course in schoology, pair it with the page ID of the corresponding page in the master DB 
-async function getCourseProjects() {
-    // get page IDs from Projects db
-    const response = await notion.databases.query({
-        database_id: process.env.NOTION_PROJECTS_DATABASE_ID,
-        filter: {
-            property: "Tags",
-            multi_select: {
-                contains: "Course",
-            },
-        },
-    })
-    return response.results;
-}
-
-// gets tasks and deadlines in the master database that are within a 7 day range by default
+/** Get tasks and deadlines in the master database that are within a 7 day range by default
+ * @param {string} startDate ISO 8601 date string of the date to start the search from
+ * @param {string} endDate ISO 8601 date string of the date to end the search at
+ * @returns {{Notion Page Resp}[]} Array of Notion pages that are in the master database and are within the given date range
+ */
 async function getEntries(startDate = util.getISODate(), endDate = util.addDaysToDate(startDate, 7)) {
     const response = await notion.databases.query({
         database_id: process.env.NOTION_MASTER_DATABASE_ID,
@@ -91,105 +33,20 @@ async function getEntries(startDate = util.getISODate(), endDate = util.addDaysT
     return response.results;
 }
 
-async function updateEntry(entryToUpdate, entryFromSchoology) {
-    const notionEntryID = entryToUpdate.id;
-
-    const response = await notion.pages.update({
-        page_id: notionEntryID,
-        properties: {
-            Date: {
-                date: {
-                    start: entryFromSchoology.date,
-                },
-            },
-        },
+/**
+ * Get a list of databases that the integration has access to
+ * @returns {Promise<{Notion Database Resp}[]>} Array of Notion databases that are shared with the integration
+ */
+function getDatabases() {
+    return notion.search({
+        filter: {
+            property: "object",
+            value: "database"
+        }
     })
-
-    return response;
-
-}
-
-// handles task creation, updates, and duplicates
-async function handleCreation(entriesFromSchoology) {
-
-    const existingEntries = await getEntries();
-
-    // check if any events are already in the master database, update duplicates if their dates are incorrect, add non duplicates
-    entries: for (let i = 0; i < entriesFromSchoology.length; i++) {
-
-        // find duplicate entries
-        const duplicateEntries = existingEntries.filter((entry) => {
-            return entry.properties.Name.title[0].plain_text === entriesFromSchoology[i].title
-        })
-
-        // skip or update duplicate entries
-        duplicates: for (let j = 0; j < duplicateEntries.length; j++) {
-
-            const assignmentDate = entriesFromSchoology[i].date;
-            const currentEntryDate = duplicateEntries[j].properties.Date.date.start;
-
-            if (assignmentDate == currentEntryDate) {
-                const timestamp = util.getISODatetime().replace(/([TZ])/g, " ").trim()
-                console.log(`[${timestamp}] Skipped creation, entry already exists: ${entriesFromSchoology[i].title}`);
-                // skip iteration of both loops
-                continue entries;
-
-            } else if (assignmentDate != currentEntryDate) {
-                // update entry
-                updateEntry(duplicateEntries[j], entriesFromSchoology[i]);
-                const timestamp = util.getISODatetime().replace(/([TZ])/g, " ").trim()
-                console.log(`[${timestamp}] Updated task: ${duplicateEntries[j].properties.Name.title[0].plain_text}`);
-                // skip iteration of both loops
-                continue entries;
-            }
-        }
-
-        // add event to new row in the master DB
-        await createRowInMaster(entriesFromSchoology[i])
-        const timestamp = util.getISODatetime().replace(/([TZ])/g, " ").trim()
-        console.log(`[${timestamp}] Successfully added entry to Notion: ${entriesFromSchoology[i].title}`);
-
-    }
-
-}
-
-function findDuplicates(sgyEntries, notionEntries) {
-    let duplicates = [];
-
-    for (let i = 0; i < sgyEntries.length; i++) {
-        for (let j = 0; j < notionEntries.length; j++) {
-            if (sgyEntries[i].title === notionEntries[j].properties.Name.title[0].plain_text) {
-                duplicates.push({
-                    sgyEntry: sgyEntries[i],
-                    notionEntry: notionEntries[j]
-                });
-            }
-        }
-    }
-    return duplicates;
-}
-
-function findDuplicatesToUpdate(duplicates) {
-    let updates = [];
-
-    for (let i = 0; i < duplicates.length; i++) {
-        const notionDate = duplicates[i].notionEntry.properties.Date.date.start;
-        const sgyDate = duplicates[i].sgyEntry.start.split(" ")[0];
-        console.log(sgyDate, notionDate);
-        if (sgyDate != notionDate) {
-            updates.push(duplicates[i].notionEntry);
-        }
-
-    }
-    return updates;
 }
 
 module.exports = {
-    createRowInMaster,
-    getCourseProjects,
     getEntries,
-    updateEntry,
-    handleCreation,
-    findDuplicates,
-    findDuplicatesToUpdate
+    getDatabases
 }
